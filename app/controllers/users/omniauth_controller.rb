11c71228972f703628e636bca15e4575f0f39ebf
bindu <bindu.devi@kloctechnologies.co.in>
2018-05-28 09:38:18 +0530

    first commit

---
+++ app/controllers/users/omniauth_controller.rb
@@ -0,0 +1,48 @@
+class Users::OmniauthController < ApplicationController
+	# facebook callback
+	def facebook
+	  @user = User.create_from_provider_data(request.env['omniauth.auth'])
+	  if @user.persisted?
+	    sign_in_and_redirect @user
+	    set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
+	  else
+	    flash[:error] = 'There was a problem signing you in through Facebook. Please register or try signing in later.'
+	    redirect_to new_user_registration_url
+	  end 
+	end
+
+	# google callback
+	def google_oauth2
+		binding.pry
+	  @user = User.create_from_provider_data(request.env['omniauth.auth'])
+	  if @user.persisted?
+	    sign_in_and_redirect @user
+	    p "111111111111111111111111111111111111111"
+	    flash[:success] = 'User got created.'
+	    # set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
+	  else
+	    flash[:error] = 'There was a problem signing you in through Google. Please register or try signing in later.'
+	    redirect_to new_user_registration_url
+	  end 
+	end
+
+	# twitter callback
+	def twitter
+		binding.pry
+	  @user = User.create_from_twitter_provider_data(request.env['omniauth.auth'])
+	  if @user.persisted?
+	  	binding.pry
+	    sign_in_and_redirect @user
+	    p "twitter..................."
+	    # set_flash_message(:notice, :success, kind: 'Twitter') if is_navigational_format?
+	  else
+	    flash[:error] = 'There was a problem signing you in through Twitter. Please register or try signing in later.'
+	    redirect_to new_user_registration_url
+	  end 
+	end
+
+	def failure
+	  flash[:error] = 'There was a problem signing you in. Please register or try signing in later.' 
+	  redirect_to new_user_registration_url
+	end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,39 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise  :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :trackable, :validatable,
+        :confirmable, :lockable, :timeoutable,
+        :omniauthable, omniauth_providers: [:github,:twitter,:facebook,:google_oauth2]
+
+  def self.create_from_provider_data(provider_data)
+      where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do | user |
+      	binding.pry
+        user.email = provider_data.info.email 
+        user.password = Devise.friendly_token[0,20]
+        user.skip_confirmation!
+      end  
+  end
+
+  def self.create_from_twitter_provider_data(provider_data)
+      where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do | user |
+        #user.email = provider_data.info[:nickname] unless provider_data.info.email
+        provider_data.info
+        user.email = provider_data.info[:nickname]+"@gmail.com" if provider_data.info.nickname
+        binding.pry
+        user.password = Devise.friendly_token[0,20]
+        user.skip_confirmation!
+      end
+  end
+
+  # def google_oauth2
+  #   @user = User.create_from_google_data(request.env['omniauth.auth'])
+  #   if @user.persisted?
+  #     sign_in_and_redirect @user
+  #     set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
+  #   else
+  #     flash[:error] = 'There was a problem signing you in through Google. Please register or try signing in later.'
+  #     redirect_to new_user_registration_url
+  #   end 
+  # end  
+end
